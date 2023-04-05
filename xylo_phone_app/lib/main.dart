7ab75f2b8fdc079dd6470fd01644584f7e798638
Ishfaqahmad0309 <121824831+Ishfaqahmad0309@users.noreply.github.com>
2023-04-05 19:37:08 +0500

    third vedio done

--- xylo_phone_app/lib/main.dart
+++ xylo_phone_app/lib/main.dart
@@ -1,64 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(XyloApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class xylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("XyloPhone"),
-        ),
-        body: Xylo(),
-      ),
+      home: Xylophone(),
     );
   }
 }
 
-class Xylo extends StatefulWidget {
-  const Xylo({Key? key}) : super(key: key);
-
-  @override
-  State<Xylo> createState() => _XyloState();
-}
-
-class _XyloState extends State<Xylo> {
-  void PlaySound(int num){
-    AssetsAudioPlayer.newPlayer().open(
-      Audio("assets/note$num.wav"),
-    );
+class Xylophone extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
   }
 
-  Expanded CreateNewButton(int sound, Color colur){
+  Expanded buildKey({required int soundNumber, required Color color}) {
     return Expanded(
-      child: Container(
-        color: colur,
-        child: TextButton(
-            onPressed: () {
-              PlaySound(sound);
-            },
-            child: Text("Button 01")),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: color,
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
       ),
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildKey(soundNumber: 1, color: Colors.red),
+            buildKey(soundNumber: 2, color: Colors.orange),
+            buildKey(soundNumber: 3, color: Colors.yellow),
+            buildKey(soundNumber: 4, color: Colors.green),
+            buildKey(soundNumber: 5, color: Colors.teal),
+            buildKey(soundNumber: 6, color: Colors.blue),
+            buildKey(soundNumber: 7, color: Colors.purple),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-        CreateNewButton(1,Colors.black26),
-        CreateNewButton(2,Colors.black12),
-        CreateNewButton(3,Colors.black26),
-      ],
+
+void main() => runApp(XyloApp());
+
+class XyloApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Xylophone(),
+    );
+  }
+}
+
+class Xylophone extends StatelessWidget {
+  void playSound(int soundNumber) {
+
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({required int soundNumber, required Color color}) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: color,
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildKey(soundNumber: 1, color: Colors.red),
+            buildKey(soundNumber: 2, color: Colors.orange),
+            buildKey(soundNumber: 3, color: Colors.yellow),
+            buildKey(soundNumber: 4, color: Colors.green),
+            buildKey(soundNumber: 5, color: Colors.teal),
+            buildKey(soundNumber: 6, color: Colors.blue),
+            buildKey(soundNumber: 7, color: Colors.purple),
+          ],
+        ),
+      ),
     );
   }
 }
